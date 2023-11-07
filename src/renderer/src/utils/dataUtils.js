287ca0a3a433cf89ee4b2cc06e8d886ef2fe6bc8
huangxulei <pjhxl@qq.com>
2023-11-07 20:11:27 +0800

    1171

--- src/renderer/src/utils/dataUtils.js
+++ src/renderer/src/utils/dataUtils.js
@@ -1,14 +1,26 @@
+//增加对应年月日的记录, record: [分钟数， 开始时间， 结束时间]
 function setRecord(data, y, m, d, record) {
-  let curr = data.record
-  curr.sum += record[0]
-  curr.count++
-  if (curr[y] == undefined) {
-    curr[y] = { sum: 0, count: 0 }
+  let curr = data.record;
+  curr.sum += record[0];
+  curr.count++;
+  if (curr[y] === undefined) {
+    curr[y] = {sum: 0, count: 0};
   }
-  curr = curr[y]
-  curr.sum += record[0]
-  curr.count++
+  curr = curr[y];
+  curr.sum += record[0];
+  curr.count++;
   if (curr[m] === undefined) {
-    curr[m] = { sum: 0, count: 0 }
+    curr[m] = {sum: 0, count: 0};
   }
+  curr = curr[m];
+  curr.sum += record[0];
+  curr.count++;
+
+  if (curr[d] === undefined) {
+    curr[d] = {sum: 0, count: 0, data: []};
+  }
+  curr = curr[d];
+  curr.sum += record[0];
+  curr.count ++;
+  curr.data.push(record);
 }
