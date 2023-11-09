a1380a8480347289add9a8fcca819eed6dae49b1
huangxulei <pjhxl@qq.com>
2023-11-09 18:01:26 +0800

    119

--- src/renderer/src/components/tomato-card/tomato-card.vue
+++ src/renderer/src/components/tomato-card/tomato-card.vue
@@ -1,6 +1,7 @@
 <script setup>
-import { ref } from 'vue';
-import { Timer } from '@element-plus/icons-vue'
+import { ref } from "vue";
+import { Timer } from "@element-plus/icons-vue";
+import musicList from './../../utils/musicList'
 
 const props = defineProps(['rest', 'alarm'])
 const emit = defineEmits(['newRecord'])
@@ -15,13 +16,28 @@ const playerText = ref('');
 //Pause闭包函数， 用于暂停音乐
 let Pause;
 
+function play(music, text) {
+    playerText.value = text;
+    dialogVisible.value = true;
+    const player = new Audio(musicList[music].value);
+    player.play()
+    const intervalId = setInterval(() => { player.play() }, 6000);
+    Pause = () => {
+        dialogVisible.value = false;
+        clearInterval(intervalId);
+        player.pause();
+    }
+}
+
 </script>
 <template>
     <el-dialog
         v-model="dialogVisible" title="Tips" width="30%">
         <span>{{ playerText }}</span>
-        <template>
-
+        <template #footer>
+            <span class="dialog-footer">
+                <el-button type="primary" @click="Pause"> 确认 </el-button>
+            </span>
         </template>
     </el-dialog>
 </template>
